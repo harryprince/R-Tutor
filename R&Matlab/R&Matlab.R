3a8bdd50d1512d1da6187175e1b461f349efc1c9
harryprince <7harryprince@gmail.com>
2014-10-05 07:44:53 +0800

    添加了 R和matlab案例
    读取matlab中读取tif文件后，数据保存为mat文件，再用R读取mat文件

--- R&Matlab/R&Matlab.R
+++ R&Matlab/R&Matlab.R
@@ -1,1 +1,14 @@
-##本文主要讲述如何让Matlab使用者迁移到R中；内容还待诸位一起补充
+##本文介绍如何读取matlab的.mat文件的方法
+##案例中的matlabtest.mat文件是事先从matlab中读取嫦娥3号月球表面100m处拍摄的数字高程图.tif文件得到的结果，保存后形成的
+
+##读取matlab文件的数据
+#install.packages('R.matlab')安装matlab包
+library(R.matlab)
+Image<-R.matlab::readMat('matlabtest.mat')##读取成功
+str(Image)##查看B中的信息
+ImageMatrix<-as.matrix(Image$B)
+
+##install.packages("ggplot2")安装 作图之神 包
+library(ggplot2)
+heatmap(ImageMatrix)##利用ggplot2包画出热力图
+##本文并未以ggplot2包作为讲解重点
